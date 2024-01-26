4c2491b315ffad4721df9eb54509531f7bd3efee
Christina Chen <omqchristi@gmail.com>
2024-01-26 15:18:02 -0800

    update

--- 22oligotemp.py
+++ 22oligotemp.py
@@ -1,8 +1,8 @@
 # 22oligotemp.py by Christina Chen
 
 def melttemp(a, t, g, c):
-	if a + t + g + c <= 13: return ((a + t) * 2 + (g + c) * 4)
-	if a + t + g + c > 13:  return (64.9 + 41 * (g + c - 16.4) / (a + t + g + c))
+	if a + t + g + c <= 13: return (a + t) * 2 + (g + c) * 4
+	if a + t + g + c > 13:  return 64.9 + 41 * (g + c - 16.4) / (a + t + g + c)
 
 print(melttemp(1, 1, 2, 2))
 print(melttemp(1, 2, 3, 4))
